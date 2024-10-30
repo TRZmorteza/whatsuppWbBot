086f3f8f525b7bd3e6eb19e0b9b27047e91e9e7d
TRZmorteza <mortezanoei80@gmail.com>
2024-10-30 08:50:22 -0700

    modified:   fileMover.py 	modified:   v3.py 	modified:   xpath.py

--- fileMover.py
+++ fileMover.py
@@ -1,10 +1,11 @@
-import os
+import readImg
 import shutil
+import os
 
 def direct():
     seek('test','t_e_s_t')
 
-B = r'C:\xampp\webdav' 
+B = r'C:\xampp\htdocs\Watsapp' 
 def make(name):
     destination_dir = os.path.join(B, name)
     if not os.path.isdir(destination_dir):
@@ -14,17 +15,18 @@ def make(name):
         print('folder for this chat already exists....')
 
 def seek(path,date):
-    fileTypeS = slice(-5, None) 
+    fileTypeS = slice(-4, None) 
     files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
     destination_dir = os.path.join(B, path,date)
     numberFiles=0
     for f in (files):
-        if f[fileTypeS] in ['.jpeg', '.jpg','.txt']: 
+
+        if f[fileTypeS] in ['jpeg', '.jpg','.txt']: 
             numberFiles+=1#count the amount if files present in download list
     if os.path.isdir(destination_dir):#check if the path is exists our not
         presentNumberFiles=0
         files = [f for f in os.listdir(destination_dir) if os.path.isfile(os.path.join(destination_dir, f))]
-        if f[fileTypeS] in ['.jpeg', '.jpg','.txt']:  
+        if f[fileTypeS] in ['jpeg', '.jpg','.txt']:  
             presentNumberFiles+=1#count amount of files presenet in save messages
         if numberFiles>presentNumberFiles:
             shutil.rmtree(destination_dir)
@@ -32,9 +34,8 @@ def seek(path,date):
             files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
             if not os.path.isdir(destination_dir):
                 os.makedirs(destination_dir, exist_ok=True)  
-                for f in (files):
-                    print(f[fileTypeS])  
-                    if f[fileTypeS] in ['.jpeg', '.jpg','.txt']: 
+                for f in (files):  
+                    if f[fileTypeS] in ['jpeg', '.jpg','.txt']: 
                         shutil.move(os.path.join(os.getcwd(), f), os.path.join(destination_dir, f))
         else:
             print('noting new here every thing is same and safe calling it a day')
@@ -44,7 +45,7 @@ def seek(path,date):
     
 def move(name,date):
     
-    fileTypeS = slice(-5, None)  
+    fileTypeS = slice(-4, None)  
 
     files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
 
@@ -52,19 +53,19 @@ def move(name,date):
     if not os.path.isdir(destination_dir):
         os.makedirs(destination_dir, exist_ok=True)  
         for f in (files):
-            print(f[fileTypeS])  
-            if f[fileTypeS] in ['.jpeg', '.jpg','.txt']: 
+             
+            if f[fileTypeS] in ['jpeg', '.jpg','.txt']: 
                 shutil.move(os.path.join(os.getcwd(), f), os.path.join(destination_dir, f))
     else:
         print('i already get this img checking the amount....')
         if seek(name,date):  
-            if f[fileTypeS] in ['.jpeg', '.jpg','.txt']: 
+            if f[fileTypeS] in ['jpeg', '.jpg','.txt']: 
                 os.makedirs(destination_dir, exist_ok=True)  
                 shutil.move(os.path.join(os.getcwd(), f), os.path.join(destination_dir, f))
         else:
             for f in (files):
                 
-                if f[fileTypeS] in ['.jpeg', '.jpg','.txt']: 
+                if f[fileTypeS] in ['jpeg', '.jpg','.txt']: 
                     os.remove(os.path.join(os.getcwd(), f))
             print('deleted the unwanted img...')
 if __name__=='__main__':
--- v3.py
+++ v3.py
@@ -3,114 +3,190 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from datetime import date
 import fileMover as F
 import xpath as X
 import time
 import os
-import pickle
-from datetime import date
+import readImg as ri
+
+# Configure Chrome options
 options = Options()
 options.add_experimental_option("prefs", {
     "download.default_directory": os.getcwd(),
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
 })
-def back():
-    act.send_keys(Keys.ESCAPE).perform()
-def s(s=5):
+def extract_number(input):
+    
+    words = input.split()
+   # print(words)
+    if  int(words[2]): 
+        return int(words[2])-int(words[0])
+    else:
+        return -1
+
+ 
+
+
+# Function to sleep for a specified number of seconds
+def sleep(s=3):
     time.sleep(s)
-#=-=-=-=-=-=-=-==-=-==-=-=-=-=-
-print('options are looded...')
-dv=webdriver.Chrome(options=options)
-act=ActionChains(dv)
-dv.get('https://web.whatsapp.com')
-with open('c.pkl', 'rb') as file:
-    cookie = pickle.load(file)
-for c in cookie:
-    c['domain']='.web.whatsapp.com'
-    try:
-     dv.add_cookie(c)
-    except Exception as e :
-        pass
-s()
-dv.get('https://web.whatsapp.com')
+# Initialize ActionChains and WebDriver
+def esc ():
+    act.send_keys(Keys.ESCAPE).perform()
+bot = webdriver.Chrome(options=options)
+act=ActionChains(bot)
+bot.get('https://web.whatsapp.com')
+sleep(10)
 
-input('press any key to contuie...')
-if chat := dv.find_elements(By.XPATH, X.chatPresent)!=[]:
-    chat=dv.find_elements(By.XPATH,X.chatPresent)
-    print('you scan the code currectly...')
-    try:
-        for C in chat:
-            print('clicking on a chat')
-            C.click()
-            F.make(dv.find_element(By.XPATH,X.chatName).text)
-            s()#sleep
-            if name:=dv.find_element(By.XPATH,X.chatName):#chat name
-                print('looking to :',name.text)
-                print('looking for imgs in this chat..')
-                if dv.find_elements(By.XPATH,X.imgs) !=[]:
-                    lanEimg=dv.find_elements(By.XPATH,X.imgs)
-                    print('loooooooooooooooooooooooooooooooook at this\n',f"{dv.find_element(By.XPATH,X.chatName).text} has imgs :",len(lanEimg))
-                    print('languge of driver is english also img stats are <TRUE>\ntrying to download')
-                    s(3)
-                    for imgF in lanEimg:
-                        try:
-                            
-                                dv.execute_script("arguments[0].scrollIntoView();", imgF)
-                                s(3)
-                                if imgF.is_displayed():
-                                    print('img is visibel')
-                                    dv.execute_script("arguments[0].click();", imgF)
-                                
-                               
-                                print('ok 55..............................................')
-                                print('clicked on img checking if its a grop our not')
-                                if dv.find_element(By.XPATH,X.singleImgE)!=None:
-                                    print('its single img trying to download')
-                                    
-                                    single=dv.find_element(By.XPATH,X.singleImgE)
-                                    single.click()
-                                    s()
-                                    F.seek(dv.find_element(By.XPATH,X.chatName).text,f"{date.today().year}_{date.today().month}_{date.today().day}")
-                                    print('clicked download button is true')
-                                    #add download move file and check here........................................
-                                    s(3)
-                                    back()    
-                                    s(2)                               
-                                
-                                    dv.execute_script("arguments[0].scrollIntoView();", imgF)
-                                    s(7)
-                                   
-                                
-                                print('its not a single')
-                                if Groupmenu:=dv.find_elements(By.XPATH,X.knewGmenu):
-                                    Groupmenu[0].click()
-                                    s(2)
-                                    groupmenuD=dv.find_element(By.XPATH,X.knewGmenuD)
-                                    if groupmenuD:
-                                        groupmenuD.click()
-                                        s(2)
-                                    back()
-                                    F.seek(dv.find_element(By.XPATH,X.chatName).text,f"{date.today().year}_{date.today().month}_{date.today().day}")
-                                        #download move here..............
-                                    back()
-                                    s(2)
-                                        
-                                
-                        except Exception as clickError:
-                            print('some thing went wrong while clicking img :',clickError)
-                            print('look with iss...')
-                            
-                            s()
-            if elementtexts:=
-                elif lanPimg:=dv.find_elements(By.XPATH,X.imgP)!=[]:
-                    print('settings on persian..')
+# Find chat elements
+chats = bot.find_elements(By.XPATH, X.chatPresent)
 
-            else:
-                print('no name found..')
-    except Exception as error:
+while True:
+    print('loading chats...')
+    for chat in chats:
+            chat.click()
+            try:
+               
+                sleep()
+                today=bot.find_element(By.XPATH,X.today)
+                act.move_to_element(today).perform()
+                sleep(5)
+                act.scroll_to_element(today).perform()
+                sleep()
+            except Exception as e:
+                act.send_keys(Keys.ESCAPE).perform()
+                print('how')
+
+    for chat in chats:
+        act.move_to_element(chat).perform()
+        sleep()
+        chat.click()
+        sleep()
+        name = bot.find_element(By.XPATH, X.chatName).text
+        F.make(name)
+        print('Looking at:', name)
         
-        print('look at it god whyyyyyyy:',error)
-        s()
-else:
-    print('no chat found')
+
+        try:
+            today=bot.find_element(By.XPATH,X.today)
+            act.scroll_to_element(today).perform()
+            sleep(6)
+            
+            try:
+                preload=bot.find_element(By.XPATH,X.downloadButtonLoad)
+                act.scroll_to_element(preload).perform()
+                sleep(6)
+                preload=bot.find_element(By.XPATH,X.downloadButtonLoad)
+                print('looking for download icon')
+                if preload:
+                    bot.execute_script("arguments[0].click();", preload)
+                    print('click on element')
+                    sleep(10) 
+            except:#done
+                print('no download button')
+                print('looking for album')
+            try:
+                
+                album=bot.find_elements(By.XPATH,X.albumImg)
+                act.scroll_to_element(album[0]).perform()
+                sleep(6)
+                print('album found:',len(album))
+                album=bot.find_elements(By.XPATH,X.albumImg)
+                if album :    
+                    for i in album:
+                        print('trying to click')
+                        act.move_to_element(i).perform()
+                        sleep(5)
+                        i.click()
+                        sleep(5)
+                        lenOfAlbum=bot.find_element(By.XPATH,X.lenOfImgs).text
+                        le=extract_number(lenOfAlbum)
+                        if le != -1:
+                            nextBn=bot.find_element(By.XPATH,X.nextButton)
+                        
+                            menuBn=bot.find_element(By.XPATH,X.downloadMenu)
+
+                            for i in range(0,le+1):
+                                sleep(5)
+                                bot.execute_script("arguments[0].click();", menuBn)
+                                sleep()
+                                try:
+                                    dtap=bot.find_element(By.XPATH,X.downloadTab)
+                                    bot.execute_script("arguments[0].click();", dtap)
+                                except:
+                                    db=bot.find_element(By.XPATH,X.downloadButton)
+                                    bot.execute_script("arguments[0].click();", db)
+                                sleep()
+                                bot.execute_script("arguments[0].click();", nextBn)
+                        else:
+                            print('no valid album find')
+                        sleep()
+                        act.send_keys(Keys.ESCAPE).perform()
+                        sleep(5)
+
+            except Exception as e:#done /3
+                print('no album img or error:',e)    
+
+            print('looking for all single imgs')
+            try:
+                img=bot.find_elements(By.XPATH,X.imgs)
+                if img :
+                    for i in img:
+                        act.scroll_to_element(i).perform()
+                        sleep(5)
+                        bot.execute_script("arguments[0].click();", i)
+                        sleep()
+                        db=bot.find_element(By.XPATH,X.downloadButton)
+                        bot.execute_script("arguments[0].click();", db)
+                        sleep()
+                        act.send_keys(Keys.ESCAPE).perform()
+                        sleep()
+
+            except:
+                print('no download button')
+            print('looking for grop messages`')
+            try:
+                sleep()
+                gText=bot.find_elements(By.XPATH,X.groptxt)
+                if  gText:
+                    filename=f"{name}_fromgGp.txt"
+                    sleep()
+                    print(len(gText),':texts found')
+                    for i in gText:
+                        act.move_to_element(i).perform()
+                        sleep()
+                        text=i.text
+                     #   print(text)
+                        with open(filename,'a',encoding='utf-8') as f:
+                            f.write(text+'\n')
+
+            except Exception as e:
+                print('no grope text our error:',e)
+            print('looking for single messages')
+            
+            try:
+                sleep()
+                sText=bot.find_elements(By.XPATH,X.shorttext)
+                if sText:
+                    print('short text find:',len(sText))
+                    filename=f"{name}_fromgGp_short.txt"
+                    print(len(sText),':texts found')
+                    for i in sText:
+                        act.move_to_element(i).perform()
+                        sleep()
+                        text=i.text
+                    #    print(text)
+
+                        with open(filename,'a',encoding='utf-8') as f:
+                            f.write(text+'\n')
+            except Exception as e:
+                print('no short text our error:',e)
+        except: 
+            print('no message found')
+      
+        F.seek(name,f"{date.today().year}_{date.today().month}_{date.today().day}")
+    chats = bot.find_elements(By.XPATH, X.chatPresent)
+    ri.maketextFile()
+    #sleep((2*60*60))# do seat sleep
