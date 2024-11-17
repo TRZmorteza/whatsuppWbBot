7860bab5ce9398b7d64b9fbfae87e33a0bfde5d4
TRZmorteza <mortezanoei80@gmail.com>
2024-11-18 00:09:13 +0330

    same name and over write done

--- fileMover.py
+++ fileMover.py
@@ -1,26 +1,11 @@
 import shutil
 import os
 
-import readd as A
 def direct():
     # reN(os.getcwd(),'my secrate')
+    for i in range(0,10):
+        open(f're{i}.txt','w').close
     seek('test','t_e_s_t')
-
-def findDop(path=os.path.join(os.getcwd(),'tempRead')):    
-    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-    for f in files:
-       
-        if ')'in f:
-            os.remove(os.path.join(path, f))
-
-
-def reN(name,date):
-    files=[f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(),f))]
-    for i,f in enumerate(files):
-        isNew,fileType=os.path.splitext(f)
-        
-        if fileType in ['.txt','.jpeg','.jpg']and isNew not in['new','result']:
-            os.rename(f,f"{name}_{date}{fileType}")
 
 B = r'C:\xampp\htdocs\Watsapp' 
 def make(name):
@@ -32,7 +17,7 @@ def make(name):
         print('folder for this chat already exists....')
 
 def seek(path,date):
-        reN(path,date)
+        # reN(path,date)
         move(path,date)
 
 
@@ -42,30 +27,57 @@ def notToDay(date,fullDate):
     os.rename('C:/xampp/htdocs/Watsapp/myfile.txt', new_path)
     return date
 
+def isthere():
+    
+    secDes=os.path.join(B,'tempRead')
+    s=slice(0,-1)
+    basefiles = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
+    files=[f for f in os.listdir(secDes) if os.path.isfile(os.path.join(secDes, f))]
+    if 0==len(files):
+        print('no file')
+        return 'break'
+    for file in basefiles:
+        for f in files:
+            fname,ftp=os.path.splitext(f)
+            perent=os.path.dirname(f)
+            if f ==file:
+                name,_=os.path.splitext(f)
+               
+                counter=0
+                while True:
 
+                    if  len(name)>2:
+                        newname=f'{name[s]}_{counter}{ftp}'
+                        if not os.path.exists(os.path.join(os.getcwd(),newname)) and not os.path.exists(os.path.join(secDes,newname)):
+                            os.rename(f,newname)
+                            return 'break'
+                    else:
+                        newname=name+'_'+str(counter)+ftp
+                        if not os.path.isdir(os.path.join(perent,newname)) :        
+                            if not os.path.exists(os.path.join(os.getcwd(),newname)) and not os.path.exists(os.path.join(secDes,newname)):
+                                os.rename(f,newname)
+                                return'break'              
+                    counter+=1
+    
 def move(name,date):
-    
-    
-
+    isthere()
     files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(os.path.join(os.getcwd(), f))]
     secDes=os.path.join(B,'tempRead')
-    print(files)
     destination_dir = os.path.join(B, name,date)
-    if not os.path.isdir(destination_dir) and not os.path.isdir(secDes):
+    if  not os.path.isdir(destination_dir) and not os.path.isdir(secDes):
         os.makedirs(destination_dir, exist_ok=True)  
         os.makedirs(os.path.join(B,'tempRead'), exist_ok=True)  
         for f in (files):
             _,Ftypes=os.path.splitext(f)       
             if Ftypes in ['.jpeg', '.jpg','.txt']: 
-                shutil.copy(os.path.join(os.getcwd(), f),os.path.join(B,'tempRead', f))
-                #shutil.move(os.path.join(os.getcwd(), f), os.path.join(destination_dir, f))
+                shutil.copy( f ,os.path.join(B,'tempRead', f))
+                shutil.move( f, os.path.join(destination_dir, f))
+
     else:                           
         for f in (files):
             _,Ftypes=os.path.splitext(f)       
-            if Ftypes in ['.jpeg', '.jpg','.txt']: 
-                shutil.copy(os.path.join(os.getcwd(), f),os.path.join(B,'tempRead', f))
-                #shutil.move(os.path.join(os.getcwd(), f), os.path.join(destination_dir, f))
-
-
+            if Ftypes in ['.jpeg', '.jpg','.txt']:
+                shutil.copy( f ,os.path.join(B,'tempRead', f))
+                shutil.move( f, os.path.join(destination_dir, f))
 if __name__=='__main__':
     direct()
